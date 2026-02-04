f5ce05f53fcf7ce93d8ce1db35ff2f81722f7b62
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-04 23:46:58 +0600

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -25,7 +25,7 @@ export function About() {
         </div>
 
         {/* Content */}
-        <div className="grid lg:grid-cols-7 gap-8 lg:gap-16 items-start">
+        <div className="grid lg:grid-cols-8 gap-8 lg:gap-16 items-start">
           {/* Image */}
           <div className="relative pb-12">
             <div 
