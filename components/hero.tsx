34a766d305f3459b6b2308ab6fe0434bfbf92301
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-08 23:55:25 +0600

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -64,7 +64,7 @@ export function Hero() {
               className="border-white/50 text-white hover:bg-white/10 px-6 py-3 h-auto bg-transparent"
             >
               Explore Now
-              <ArrowUpRight className="ml-20 h-4 w-4" />
+              <ArrowUpRight className="ml-21 h-4 w-4" />
             </Button>
           </div>
         </div>
