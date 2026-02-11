b9827f11772df37e6974969f13435f8440d679ea
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-11 23:33:52 +0600

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -94,7 +94,7 @@ export function Hero() {
               PREV
             </button>
             
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-5">
               {slides.map((_, index) => (
                 <button
                   key={index}
