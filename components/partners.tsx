e495214e7f7e4d08c94a3cb2ddfaeeb7795485c4
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-13 23:38:29 +0600

    Update partners.tsx

--- components/partners.tsx
+++ components/partners.tsx
@@ -7,7 +7,7 @@ const partners = [
     name: "H&M",
     logo: (
       <svg viewBox="0 0 100 50" className="h-12 w-auto">
-        <text x="25" y="40" className="fill-red-600 text-4xl font-bold" style={{ fontFamily: 'serif' }}>
+        <text x="26" y="40" className="fill-red-600 text-4xl font-bold" style={{ fontFamily: 'serif' }}>
           H&M
         </text>
       </svg>
