af26cb1212c0ce37b919d454016c9aa9d0ec754f
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-23 23:52:45 +0600

    Update portfolio.tsx

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -96,7 +96,7 @@ export function Portfolio() {
           </div>
 
           {/* Right Column */}
-          <div className="space-y-4 lg:space-y-6">
+          <div className="space-y-4 lg:space-y-7">
             {rightColumnItems.map((item, index) => (
               <div key={index} className="group">
                 <div className={`${item.aspectRatio} bg-neutral-200 overflow-hidden relative`}>
