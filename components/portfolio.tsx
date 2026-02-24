e4811d60f67011eb74c4fda4e7a4c8c4c2ef8570
Rajkumar Sarker <sardonic100@gmail.com>
2026-02-24 23:51:09 +0600

    Update portfolio.tsx

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -87,7 +87,7 @@ export function Portfolio() {
                     className="object-cover"
                   />
                 </div>
-                <div className="mt-11">
+                <div className="mt-12">
                   <h3 className="text-sm font-semibold text-foreground">{item.title}</h3>
                   <p className="text-xs text-muted-foreground">{item.author}</p>
                 </div>
