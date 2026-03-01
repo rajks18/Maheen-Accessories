634dc9aa9adce10872d7a9b297cce730b9343176
Rajkumar Sarker <sardonic100@gmail.com>
2026-03-01 23:30:58 +0600

    Update why-choose-us.tsx

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -22,7 +22,7 @@ export function WhyChooseUs() {
             </h2>
 
             {/* Features List */}
-            <div className="space-y-14">
+            <div className="space-y-15">
               {features.map((feature, index) => (
                 <div
                   key={index}
