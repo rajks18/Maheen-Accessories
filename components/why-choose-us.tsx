473c08d14f108df059b92743db75b6a19b74842d
Rajkumar Sarker <sardonic100@gmail.com>
2026-03-10 23:33:28 +0600

    Update why-choose-us.tsx

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -40,7 +40,7 @@ export function WhyChooseUs() {
           </div>
 
           {/* Right Content - Building Image with Card */}
-          <div className="relative pb-25">
+          <div className="relative pb-26">
             {/* Building Image */}
             <div className="aspect-[4/3] rounded-lg overflow-visible relative">
               <img 
